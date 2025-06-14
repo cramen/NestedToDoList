7ccf397fec465bcaeecd3718a3977fd2ceba4df6
cramen <cramen@cramen.ru>
2025-06-15 00:35:50 +0300

    linter fixes

--- src/components/TaskView.tsx
+++ src/components/TaskView.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Task, CreateTaskRequest, UpdateTaskRequest } from '../types/Task';
 import { TaskForm } from './TaskForm';
 import { TaskList } from './TaskList';
