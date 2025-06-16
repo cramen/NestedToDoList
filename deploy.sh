03c6f02ea0711f075c1f83e8220dc440f0778a55
cramen <cramen@cramen.ru>
2025-06-16 20:59:52 +0300

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
-git push origin --delete gh-pages && rm -rf node_modules/.cache/gh-pages && npm run deploy
+git push origin --delete gh-pages
+rm -rf node_modules/.cache/gh-pages && npm run deploy
